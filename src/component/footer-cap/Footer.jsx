afbb46f8c309274075988f409590e7c9107d40ea
FranciscaRuzV <fpazruz0422@gmail.com>
2023-10-18 16:09:42 -0300

    component y pages educacionSection

---
+++ src/component/footer-cap/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <section className="footer">
+      <div className="box-container">
+        <div className="box">
+          <h3>Links</h3>
+          <a href="#">Inicio</a>
+          <a href="#">Sobre Nosotros</a>
+          <a href="#">La Comunidad</a>
+          <a href="#">Educación</a>
+        </div>
+        <div className="box">
+          <h3>Extra links</h3>
+          <a href="#">Mi cuenta</a>
+          <a href="#">La Comunidad</a>
+          <a href="#">Mapa Compost</a>
+        </div>
+        <div className="box">
+          <h3>Contacto</h3>
+          <a href="mailto:compostemos@comp.com">compostemos@comp.com</a>
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="credit">&copy; Compostemos</div>
+    </section>
+  );
+}
---
+++ src/pages/EducacionSection.jsx
@@ -0,0 +1,67 @@
+import "./EducacionSection.css";
+export default function EducacionSection() {
+  return (
+    <div className="container-educacion">
+      <div className="intro" id="intro">
+        <div className="intro-content">
+          <h3>¡Aprendamos sobre compostaje!</h3>
+          <span>Un panorama para cambiar el mundo: tú y yo, compostando.</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            cumque quam eum autem quasi accusantium suscipit ad sint dolor
+            dolore, nostrum perspiciatis ipsa in similique obcaecati totam
+            culpa? Repudiandae, aliquam?
+          </p>
+        </div>
+      </div>
+      <div className="intro" id="intro">
+        <div className="intro-content">
+          <h3>¿Qué es el compostaje?</h3>
+          <span>Subtitulo?</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            cumque quam eum autem quasi accusantium suscipit ad sint dolor
+            dolore, nostrum perspiciatis ipsa in similique obcaecati totam
+            culpa? Repudiandae, aliquam?
+          </p>
+        </div>
+      </div>
+      <div className="intro" id="intro">
+        <div className="intro-content">
+          <h3>Tipos de compostaje</h3>
+          <span>Subtitulo?</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            cumque quam eum autem quasi accusantium suscipit ad sint dolor
+            dolore, nostrum perspiciatis ipsa in similique obcaecati totam
+            culpa? Repudiandae, aliquam?
+          </p>
+        </div>
+      </div>
+      <div className="intro" id="intro">
+        <div className="intro-content">
+          <h3>Mitos</h3>
+          <span>Subtitulo?</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            cumque quam eum autem quasi accusantium suscipit ad sint dolor
+            dolore, nostrum perspiciatis ipsa in similique obcaecati totam
+            culpa? Repudiandae, aliquam?
+          </p>
+        </div>
+      </div>
+      <div className="intro" id="intro">
+        <div className="intro-content">
+          <h3>¿Cómo puedo comenzar?</h3>
+          <span>Subtitulo?</span>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
+            cumque quam eum autem quasi accusantium suscipit ad sint dolor
+            dolore, nostrum perspiciatis ipsa in similique obcaecati totam
+            culpa? Repudiandae, aliquam?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
