28dc9543d2c7aea41a6e8fe8c3d6f65ed3ef276b
FranciscaRuzV <fpazruz0422@gmail.com>
2023-10-18 16:34:49 -0300

    otro commit de package y appjs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Header from "./component/Header";
+import Header from "./component/header-cap/Header";
 import InicioSection from "./pages/InicioSection";
 import SobreNosotrosSection from "./pages/SobreNosotrosSection";
-import Footer from "./component/Footer";
-import { Route, Routes, Link } from "react-router-dom";
 import Comunidad from "./pages/Comunidad";
+import EducacionSection from "./pages/EducacionSection";
+import Footer from "./component/footer-cap/Footer";
+import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
@@ -11,9 +12,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<InicioSection />} />
-
         <Route path="/sobre-nosotros" element={<SobreNosotrosSection />} />
         <Route path="/comunidad" element={<Comunidad />} />
+        <Route path="/educacion" element={<EducacionSection />} />
       </Routes>
       <Footer />
     </div>
