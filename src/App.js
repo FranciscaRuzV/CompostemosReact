c5696710c73f0d4a9dbfcb167754213ed7fa2c3f
FranciscaRuzV <fpazruz0422@gmail.com>
2023-10-19 12:28:19 -0300

    cambios del proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Header from "./component/header-cap/Header";
+import Header from "./component/hederCap/Header";
 import InicioSection from "./pages/InicioSection";
 import SobreNosotrosSection from "./pages/SobreNosotrosSection";
 import Comunidad from "./pages/Comunidad";
 import EducacionSection from "./pages/EducacionSection";
-import Footer from "./component/footer-cap/Footer";
+import Footer from "./component/foterCap/Footer";
+import LoginSection from "./pages/LoginSection";
 import { Route, Routes, Link } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/sobre-nosotros" element={<SobreNosotrosSection />} />
         <Route path="/comunidad" element={<Comunidad />} />
         <Route path="/educacion" element={<EducacionSection />} />
+        <Route path="/login" element={<LoginSection />} />
       </Routes>
       <Footer />
     </div>
--- src/component/hederCap/Header.jsx
+++ src/component/hederCap/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
@@ -7,16 +8,16 @@ export default function Header() {
       <input type="checkbox" name="" id="toggler" />
       <label htmlFor="toggler" className="fas fa-bars"></label>
 
-      <a href="#inicio" className="logo">
+      <Link to="/" className="logo">
         Compostemos <span>.</span>
-      </a>
+      </Link>
 
       <nav className="navbar">
         <Link to="/">Inicio</Link>
         <Link to="/sobre-nosotros">Sobre Nosotros</Link>
         <Link to="/comunidad">La Comunidad</Link>
-        <a href="#">Educación</a>
-        <a href="#"></a>
+        <Link to="/educacion">Educación</Link>
+        <Link to="/login">Login</Link>
       </nav>
       <div className="icons">
         <a href="./login.html" className="fas fa-user"></a>
